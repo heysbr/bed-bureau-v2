86a594792a2af8cdf4d456b1ebe5d951854f2e51
Priyanshu Sabaar <random.priyanshu@gmail.com>
2025-09-24 13:02:22 +0530

    style: improve layout and styling of login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -35,21 +35,21 @@ export default function Page() {
   }
 
   return (
-    <div className="relative h-screen bg-app-bg flex justify-around md:px-20 md:items-center ">
+    <div className="relative h-screen bg-app-bg flex md:px-20 md:items-center ">
       <Image src={Logo} alt="logo" className="absolute top-5 left-25"  />
-      <div className="hidden md:flex flex-col items-center justify-center">
+      <div className="flex-1 hidden md:flex flex-col items-center justify-center">
         <span className="text-4xl m-5">Welcom to 
         <br />
         <span className="font-semibold text-[#684ABC]">Bed Bureau</span></span>
         <Image src={BgImage} alt="bg-image" />
       </div>
 
-      <div className="flex flex-col w-full px-5 md:h-full gap-y-5 py-20 md:items-center md:justify-center">
+      <div className="flex flex-col md:h-full gap-y-5 p-5 w-full md:w-fit py-20 md:items-end  md:justify-center">
         <PageTitle className="md:hidden" />
         <form
           onSubmit={handleSubmit(submit)}
           // className="flex flex-col gap-y-10 px-12 bg-app-form-bg p-5 shadow-[#DFE5F0] rounded shadow-[0px_4px_15px_0px_#DFE5F0] md:w-md h-3/5"
-          className="flex flex-col bg-white md:p-20 p-5 space-y-2 md:space-y-6 w-full md:w-125 rounded-lg shadow-[0px_4px_15px_0px_#DFE5F0]"
+          className="flex flex-col bg-white md:p-20 border p-5 space-y-2 md:space-y-6 w-full md:w-125 rounded-lg shadow-[0px_4px_15px_0px_#DFE5F0]"
         >
           <Heading className="hidden md:block text-2xl font-semibold">Login</Heading>
           {LoginFields.map((field, index) => (
